558270ec627e0d688eaf6fbdd22450ecaca92bbb
Gál Péter <pepyta118@gmail.com>
2020-11-05 09:02:28 +0100

    Google auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth, { InitOptions } from 'next-auth';
+import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient(); 
+
+const options: InitOptions = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+    })
+  ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+}
+
+export default (req, res) => NextAuth(req, res, options)
